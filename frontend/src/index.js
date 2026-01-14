1e7d732955f1e091c904e52123258bd45b46ae60
Sota Kato <249757498+sotakato-arch@users.noreply.github.com>
2026-01-14 15:28:07 +0900

    instrument frontend faro

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
+
+initializeFaro({
+  url: 'https://faro-collector-prod-ap-northeast-0.grafana.net/collect/5ba52c6577c8440163e3855d1e21b34d',
+  app: {
+    name: 'Flight Sim',
+    version: '1.0.0',
+    environment: 'production'
+  },
+
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+  ],
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
